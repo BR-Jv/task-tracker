93cd35eff47633f62a1755a8dd704a8e9e211fb1
BR-Jv <jv.alves193@gmail.com>
2026-07-20 17:01:30 -0300

    WIP: implementa a classe Tracker

--- src/service/Tracker.php
+++ src/service/Tracker.php
@@ -2,9 +2,13 @@
     require('./src/service/DataManager.php');
 
     class Tracker {
-        
 
-        
+        private $manager;
+
+        function __construct()
+        {
+        }
+
 
         function add()
         {
